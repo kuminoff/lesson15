6f615c10c3574fcb22e1b218ab5c13d097352ed5
Viacheslav Kuminov <kuminov200220@gmail.com>
2022-12-21 17:55:57 +0700

    15 complited

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const DomElement = function (select, hei, wid, back, font) {
   this.width = wid;
   this.bg = back;
   this.fontSize = font;
+  this.left = 50;
+  this.top = 50;
 
-  this.createElem = () => {
+  (this.createElem = () => {
     const elem =
       String(this.selector)[0] === "."
         ? document.createElement("div")
@@ -22,13 +24,41 @@ const DomElement = function (select, hei, wid, back, font) {
           this.selector.slice(1)
         )}">Id данного параграфа - '${String(this.selector.slice(1))}'<p>`);
     body.appendChild(elem);
-    console.log(elem);
     elem.style.cssText = `height: ${this.height};
         width: ${this.width}; background-color: ${this.bg};
-        font-size: ${this.fontSize}; `;
-  };
+        font-size: ${this.fontSize}; position: absolute; left: ${this.left}px; top: ${this.top}px;`;
+  }),
+    (this.changePosition = (e) => {
+      console.log(e.code);
+      switch (true) {
+        case e.code === "ArrowDown":
+          this.top += 10;
+          console.log(this.top);
+          body.innerHTML = "";
+          this.createElem();
+          break;
+        case e.code === "ArrowUp":
+          this.top -= 10;
+          console.log(this.top);
+          body.innerHTML = "";
+          this.createElem();
+          break;
+        case e.code === "ArrowLeft":
+          this.left -= 10;
+          body.innerHTML = "";
+          this.createElem();
+          console.log(this.left);
+          break;
+        case e.code === "ArrowRight":
+          this.left += 10;
+          body.innerHTML = "";
+          this.createElem();
+          console.log(this.left);
+          break;
+      }
+    });
 };
 
 const newElem = new DomElement(".que", "100px", "100px", "blue", "14px");
-
-newElem.createElem();
+document.addEventListener("DOMContentLoaded", newElem.createElem());
+document.addEventListener("keydown", newElem.changePosition);
